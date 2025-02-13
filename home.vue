e7af36662092537e5604846d96d55a97d15f6507
rahim8050 <rahimranxx8050@gmail.com>
2025-02-13 10:30:10 +0000

    trip

---
+++ home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="home">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is a simple Vue.js home page.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            message: 'Hello, Vue!'
+        };
+    }
+};
+</script>
+
+<style scoped>
+.home {
+    text-align: center;
+    margin-top: 50px;
+}
+
+h1 {
+    color: #42b983;
+}
+
+p {
+    font-size: 18px;
+}
+</style></div></template>
